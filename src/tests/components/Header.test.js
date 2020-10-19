c172b3cd0887880bc1645204090fd0ffd024eb2a
Patric Gekoski <PatricGekoski@protonmail.com>
2020-10-19 12:34:15 -0400

    add snapshotSerializers to jest.config.json

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render header correctly', () => {
-	const renderer = new ReactShallowRenderer();
-   renderer.render(<Header />);
-   expect(renderer.getRenderOutput()).toMatchSnapshot();
-	console.log(renderer.getRenderOutput());
+	// const renderer = new ReactShallowRenderer();
+	// renderer.render(<Header />);
+	// expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+	// with Enzyme
+	const wrapper = shallow(<Header />);
+	expect(toJSON(wrapper)).toMatchSnapshot();
+	// expect(wrapper.find('h1').text()).toBe('Expensify');
 });
