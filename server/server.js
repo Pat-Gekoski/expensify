acf855e88470c55f6efd8ecc832e04d189a88ed2
Patric Gekoski <PatricGekoski@protonmail.com>
2020-10-23 18:46:21 -0400

    fixed bug with server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-const PORT = proces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log(`server is up and running on port ${PORT}`);
 });
